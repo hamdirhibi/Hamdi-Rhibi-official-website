279f5b92e050f5f13264bae0092cd354d214c350
hamdirhibi <geekrhibi@gmail.com>
2020-04-09 02:33:07 +0100

    fourth one

--- send_mail.php
+++ send_mail.php
@@ -11,7 +11,7 @@
       require_once 'vendor/autoload.php' ; 
 
 
-      $transport = (new Swift_SmtpTransport('smtp.gmail.com',587,'tls'))
+      $transport = (new Swift_SmtpTransport('smtp.gmail.com',465,'tls'))
        ->setUsername('codeinthedarktunisia@gmail.com')
        ->setPassword('codeinthedark'); 
 
